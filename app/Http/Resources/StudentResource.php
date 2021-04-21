b22d38d9b64e7ec0370c0881668e126b58207015
refinaldy7 <refimadras.code@gmail.com>
2021-04-21 22:03:53 +0700

    bug fixing

--- app/Http/Resources/StudentResource.php
+++ app/Http/Resources/StudentResource.php
@@ -27,7 +27,7 @@ class StudentResource extends JsonResource
             'jurusan' => $this->major,
             'id_tahun_akademik' => $this->academic_year_id,
             'foto_siswa' => $this->image,
-            'url_foto' => asset('/images/foto_siswa/' . $this->image),
+            'url_foto' => asset('/images/student_images/' . $this->image),
             'ijazah' => $this->ijazah_file,
             'skhun' => $this->skhun_file,
             'tahun_lulus' => $this->academic_year->year_start . '/' . $this->academic_year->year_end
