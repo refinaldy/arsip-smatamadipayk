c12fe4a46aceafc10d4e9a06e49ac3c045eef894
refinaldy7 <refimadras.code@gmail.com>
2021-04-25 19:40:16 +0700

    fix format image

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -368,7 +368,7 @@ class StudentController extends Controller
             'jurusan' => 'required',
             'id_tahun' => 'required',
             'tanggal_lahir' => 'required',
-            'foto_siswa' => 'mimes:image/jpg,image/png,jpg,png|max:2048'
+            'foto_siswa' => 'mimes:jpg,png, jpeg|max:2048'
         ];
     }
 
